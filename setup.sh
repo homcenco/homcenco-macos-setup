ffb39d27d6f919c82a857f1440547dd2d415f91b
Vasili Homcenco <homcenco@gmail.com>
2023-09-03 20:51:44 +0300

    feat(setup): change composer version to 2.6.2

--- setup.sh
+++ setup.sh
@@ -84,7 +84,7 @@ function setup_php_env() {
   alert "Rebuild composer non-political:"
   local COMPOSER_TEMP="${HOME}/composer-build"
   [ -f "${COMPOSER_TEMP}" ] && rm -rf "${COMPOSER_TEMP}"
-  git clone https://github.com/composer/composer.git --branch 2.6.1  "${COMPOSER_TEMP}" && \
+  git clone https://github.com/composer/composer.git --branch 2.6.2  "${COMPOSER_TEMP}" && \
       composer install -o -d "${COMPOSER_TEMP}" && \
       wget https://raw.githubusercontent.com/politsin/snipets/master/patch/composer.patch -q -O "${COMPOSER_TEMP}/composer.patch"  && \
       cd "${COMPOSER_TEMP}" && patch -p1 < composer.patch && \
